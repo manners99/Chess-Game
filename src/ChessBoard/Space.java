cd6d07546c40b9306b73887605a1abd6a36e2246
Joshua <jman988@aucklanduni.ac.nz>
2023-06-21 15:48:41 +1200

    Space class initialized

--- src/ChessBoard/Space.java
+++ src/ChessBoard/Space.java
@@ -1,5 +1,41 @@
 package ChessBoard;
 
+import Pieces.Piece;
+
 public class Space {
+    
+    private Piece piece;
+    private int X;
+    private int Y;
+
+    public Space(Piece piece, int X, int Y) {
+        this.piece = piece;
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
     
 }
